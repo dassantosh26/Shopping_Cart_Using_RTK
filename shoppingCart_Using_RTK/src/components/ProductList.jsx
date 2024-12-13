15206940ae156f067a1e3bfeef4025b98332ad67
Santosh Kumar Das <santoshdas.mits@gmail.com>
2024-12-13 10:25:55 +0530

    Fetched and displayed product using fetch api

--- shoppingCart_Using_RTK/src/components/ProductList.jsx
+++ shoppingCart_Using_RTK/src/components/ProductList.jsx
@@ -1,16 +1,31 @@
 /** @format */
+import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 const ProductList = () => {
+  const [products,setProducts]=useState([])
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      // console.log(data);  
+      setProducts(data)
+    }
+    fetchProducts()
+  },[])
   return (
     <>
       <Navbar />
       <div className="product-list">
-        <div className="product-card">
-          <img src="image path" alt="image title" />
-          <h2>Product Title</h2>
-          <p>Price: $200</p>
-          <button>Add to Cart</button>
-        </div>
+        {
+          products.map((product) => (
+            <div className="product-card" key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <h2>{product.title.length>20?`${product.title.slice(0,20)}...`:product.title}</h2>
+            <p>Price: ${product.price}</p>
+            <button>Add to Cart</button>
+          </div>
+          ))
+      }
       </div>
     </>
   );
