c41bf6b09e53e046a32210cc756e40b567271b70
Santosh Kumar Das <santoshdas.mits@gmail.com>
2024-12-14 13:12:09 +0530

    Data fetched from api using createAsyncThunk

--- shoppingCart_Using_RTK/src/components/ProductList.jsx
+++ shoppingCart_Using_RTK/src/components/ProductList.jsx
@@ -1,31 +1,48 @@
 /** @format */
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Navbar from "./Navbar";
+import { fetchProducts } from "../features/ShopCart/productSlice";
+
 const ProductList = () => {
-  const [products,setProducts]=useState([])
+  // useSelector(state=>console.log(state.products)
+  // )
+  const { items: products, status } = useSelector((state) => state.products);
+
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    const fetchProducts = async () => {
+    /* const fetchProducts = async () => {
       const res = await fetch("https://fakestoreapi.com/products");
       const data = await res.json();
       // console.log(data);  
       setProducts(data)
     }
-    fetchProducts()
-  },[])
+    fetchProducts() */
+
+    if (status === "idle") {
+      dispatch(fetchProducts());
+    }
+  }, [status]);
+  if (status === "loading") return <p>Loading Products ...</p>;
+  if (status === "failed") return <p>Failed to load products.Please try again ...</p>;
+
   return (
     <>
       <Navbar />
       <div className="product-list">
-        {
-          products.map((product) => (
-            <div className="product-card" key={product.id}>
+        {products.map((product) => (
+          <div className="product-card" key={product.id}>
             <img src={product.image} alt={product.title} />
-            <h2>{product.title.length>20?`${product.title.slice(0,20)}...`:product.title}</h2>
+            <h2>
+              {product.title.length > 20
+                ? `${product.title.slice(0, 20)}...`
+                : product.title}
+            </h2>
             <p>Price: ${product.price}</p>
             <button>Add to Cart</button>
           </div>
-          ))
-      }
+        ))}
       </div>
     </>
   );
